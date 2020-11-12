d17559f7083ff43a2b151f961a5d937804e361c8
fayekollig <kolligfa@msu.edu>
2020-11-12 12:19:57 -0500

    ssss

--- soundboard.js
+++ soundboard.js
@@ -1,10 +1,11 @@
 
-var blip = new Audio("blip.wav")
-var bloop = new Audio("bloop.wav")
-var blurp = new Audio("blurp.wav")
 blipButton = document.getElementById('blipButton')
 bloopButton = document.getElementById('bloopButton')
 blurpButton = document.getElementById('blurpButton')
+
+var blip = new Audio("blip.wav")
+var bloop = new Audio("bloop.wav")
+var blurp = new Audio("blurp.wav")
 
 blipButton.addEventListener('click', blip.play())
 bloopButton.addEventListener('click', bloop.play())
